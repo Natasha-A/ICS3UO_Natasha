3ae012c27c4c8749776cd2ae47d737ff14adf14c
Natasha Ahammed <natasha.ahammed@outlook.com>
2019-12-19 21:19:27 -0500

    'Enemy' object is able to choose a pokemon at random, with user information displayed from Pokemon()

--- Computer Science/ICS 3U - Summative/SummativeCS.py
+++ Computer Science/ICS 3U - Summative/SummativeCS.py
@@ -1,8 +1,9 @@
-# standard structures - may need to include different kinds under super class
-class pokemonMaster():
+# CLASS OBJECTS
+import random
+
+class Pokemon():
     def __init__(self, name="", health=100, kind="", attackOne="", attackTwo="", attackThree=""):
-        # if necessary try to include
-        # default vals and list type
+        self.allPokemon = []
         self.name = name
         self.health = health
         self.kind = kind
@@ -16,33 +17,46 @@ class pokemonMaster():
                                                                                       self.attackOne, self.attackTwo,
                                                                                       self.attackThree)
 
-
-class Player(pokemonMaster):
-    def __init__(self, name, health, kind, attackOne, attackTwo, attackThree):
-        # if necessary try to include
-        # default values (must include all attributes) and list type
+class Player(Pokemon):
+    def __init__(self, name="", health=100, kind="", attackOne="", attackTwo="", attackThree=""):
         super().__init__(name, health, kind, attackOne, attackTwo, attackThree)
 
-        '''
-        self.name = name
-        self.health = health
-        self.type = type
-        self.attackOne = attackOne  # eventually condense into one list
-        self.attackTwo = attackTwo
-        self.attackThree = attackThree
-        '''
 
+class Enemy(Pokemon):
+    def __init__(self, name="", health=100, kind="", attackOne="", attackTwo="", attackThree=""):
+        super().__init__(name, health, kind, attackOne, attackTwo, attackThree)
+
+    #Enemy chooses pokemon at random
+    def choosePokemon(self):
+        pokemonChosen = random.choice(pokemons)
+
+        nameOfPokemon = pokemonChosen.name
+
+        print("The random choice chosen is", nameOfPokemon)
+
+        return pokemonChosen
+
+#LIST OF POKEMON - instances
+charmander = Pokemon("Charmander", 100,"Fire", "whip", "blaze", "shock")
+bulbasaur = Pokemon("Bulbasaur",100, "Leaf", "vine", "levitate", "run away")
+squirtle = Pokemon("Squirtle",100, "hydro pump", "final smash", "triple finish")
+
+pokemons = [charmander, bulbasaur, squirtle]
 
 '''
-class Enemy(pokemonMaster)
-    def __init__(self, name, health, type, attackOne, attackTwo, attackThree):
-        # if necessary try to include
-        # default values (must include all attributes) and list type
-        super().__init__(self,name,health,type
-                       ,attackOne,attackTwo,attackThree)
+1)	AI GENEREATES random Pokemon ():
+•	CHOOSES from Pokemon Instances LIST (RANDOM)
+•	PRINT AI's Pokemon chosen (indexed)
+•	RETURN Pokemon chosen (INSTANCE)
 '''
-playerOne = Player("Charmander",100, "Fire", "whip", "blaze", "shock")
-print(playerOne.playerInfo())
 
-# enemy = Enemy("Bulbasaur","Leaf", "vine", "levitate", "run away")
-# print(enemy.playerInfo())
+#create instance of an enemy - takes new enemy, and prints out it's information
+enemyOne = Enemy()
+
+enemyPokemon = Enemy.choosePokemon(enemyOne)
+
+print(enemyPokemon.playerInfo()) #derives from Pokemon() object
+
+
+
+
