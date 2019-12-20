c72234228dfa4f85c2ec63fab06f58e651ab5d4c
Natasha Ahammed <natasha.ahammed@outlook.com>
2019-12-19 19:48:41 -0500

    Player and enemy can now inherit from super class created

--- Computer Science/ICS 3U - Summative/SummativeCS.py
+++ Computer Science/ICS 3U - Summative/SummativeCS.py
@@ -1,43 +1,48 @@
-
-#standard structures - may need to include different kinds under super class
-class Player():
-    def __init__(self, name="", health=100, type="", attackOne="", attackTwo="", attackThree=""): #if necessary try to include
+# standard structures - may need to include different kinds under super class
+class pokemonMaster():
+    def __init__(self, name="", health=100, kind="", attackOne="", attackTwo="", attackThree=""):
+        # if necessary try to include
         # default vals and list type
         self.name = name
         self.health = health
-        self.type = type
+        self.kind = kind
         self.attackOne = attackOne
         self.attackTwo = attackTwo
         self.attackThree = attackThree
 
-
     def playerInfo(self):
         return 'Name:{} Health:{} Type:{} Attack 1:{} Attack 2:{} Attack 3:{}'.format(self.name,
-                                            self.health, self.type, self.attackOne, self.attackTwo, self.attackThree)
+                                                                                      self.health, self.kind,
+                                                                                      self.attackOne, self.attackTwo,
+                                                                                      self.attackThree)
 
-class Enemy(Player):#inherits from super class of Player() UNFINISHED 
-    def __init__(self, name="", health=100, type="", attackOne="", attackTwo="", attackThree=""):  # if necessary try to include
+
+class Player(pokemonMaster):
+    def __init__(self, name, health, kind, attackOne, attackTwo, attackThree):
+        # if necessary try to include
         # default values (must include all attributes) and list type
+        super().__init__(name, health, kind, attackOne, attackTwo, attackThree)
+
+        '''
         self.name = name
         self.health = health
         self.type = type
         self.attackOne = attackOne  # eventually condense into one list
         self.attackTwo = attackTwo
         self.attackThree = attackThree
+        '''
 
-    def playerInfo(self):
-        return 'Name:{} Health:{} Type:{} Attack 1:{} Attack 2:{} Attack 3:{}'.format(self.name,
-                                                                                      self.health, self.type,
-                                                                                      self.attackOne, self.attackTwo,
-                                                                                      self.attackThree
-playerOne = Player("Charmander", "Fire","whip","blaze","shock")
+
+'''
+class Enemy(pokemonMaster)
+    def __init__(self, name, health, type, attackOne, attackTwo, attackThree):
+        # if necessary try to include
+        # default values (must include all attributes) and list type
+        super().__init__(self,name,health,type
+                       ,attackOne,attackTwo,attackThree)
+'''
+playerOne = Player("Charmander",100, "Fire", "whip", "blaze", "shock")
 print(playerOne.playerInfo())
 
-enemy = Enemy("Bulbasaur","Leaf", "vine", "levitate", "run away")
-print(enemy.playerInfo())
-
-
-
-
-
-
+# enemy = Enemy("Bulbasaur","Leaf", "vine", "levitate", "run away")
+# print(enemy.playerInfo())
